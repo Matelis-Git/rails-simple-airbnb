f7968b73df9fccb73d5c43e7d376189e632e4cf8
Matteo Dauba <matteo.elyas@gmail.com>
2026-05-20 16:59:41 +0200

    shitlooksclean

--- Gemfile
+++ Gemfile
@@ -63,3 +63,7 @@ group :test do
 end
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem 'rspec-rails', group: [ :test ]
+gem 'rails-controller-testing', group: [ :test ]
+
+gem "simple_form", "~> 5.4"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750'
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'A stylish and modern house just minutes from the River Thames.',
+  price_per_night: 65,
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb'
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'An amazing guest suite inside the iconic St Pancras building.',
+  price_per_night: 110,
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688'
+)
+
+Flat.create!(
+  name: 'Cosy Notting Hill Studio',
+  address: '27 Portobello Rd London W11 1LU',
+  description: 'A cosy studio in the heart of Notting Hill.',
+  price_per_night: 90,
+  number_of_guests: 1,
+  picture_url: 'https://images.unsplash.com/photo-1484154218962-a197022b5858'
+)
